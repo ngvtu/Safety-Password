0584e384ed5f834c2e9aa3d868701a14fe8b37ed
Nguyễn Văn Tú <nvtu1612@gmail.com>
2023-07-04 23:51:38 +0700

    Create project

---
+++ app/src/main/java/com/example/safety_password/views/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.safety_password.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.safety_password.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
